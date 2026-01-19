b0bbb0f5b9612378ae9ad14b3bca2669011ef34e
winter.loo <winterloo@126.com>
2026-01-19 16:01:19 +0800

    integrate serf for membership detection

--- go.mod
+++ go.mod
@@ -13,16 +13,30 @@ require (
 )
 
 require (
+	github.com/armon/go-metrics v0.4.1 // indirect
 	github.com/bmatcuk/doublestar/v4 v4.6.1 // indirect
 	github.com/casbin/govaluate v1.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/btree v1.1.3 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.3.3 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-metrics v0.5.4 // indirect
+	github.com/hashicorp/go-msgpack/v2 v2.1.5 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.7 // indirect
+	github.com/hashicorp/golang-lru v1.0.2 // indirect
+	github.com/hashicorp/memberlist v0.5.4 // indirect
+	github.com/hashicorp/serf v0.10.2 // indirect
+	github.com/miekg/dns v1.1.68 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sean-/seed v0.0.0-20170313163322-e2103e2c3529 // indirect
+	github.com/travisjeffery/go-dynaport v1.0.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.64.0 // indirect
 	go.opentelemetry.io/otel v1.39.0 // indirect
@@ -34,8 +48,11 @@ require (
 	go.opentelemetry.io/otel/trace v1.39.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.27.1 // indirect
+	golang.org/x/mod v0.29.0 // indirect
 	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sync v0.18.0 // indirect
 	golang.org/x/sys v0.39.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
+	golang.org/x/tools v0.38.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/discovery/membership.go
@@ -0,0 +1,138 @@
+package discovery
+
+import (
+	"net"
+
+	"github.com/hashicorp/serf/serf"
+	"go.uber.org/zap"
+)
+
+type Membership struct {
+	Config
+	handler Handler
+
+	serf   *serf.Serf
+	events chan serf.Event
+	logger *zap.Logger
+}
+
+type Config struct {
+	NodeName       string
+	BindAddr       string
+	Tags           map[string]string
+	StartJoinAddrs []string
+}
+
+type Handler interface {
+	Join(name, addr string) error
+	Leave(name string) error
+}
+
+func New(handler Handler, config Config) (*Membership, error) {
+	c := &Membership{
+		Config:  config,
+		handler: handler,
+		logger:  zap.L().Named("membership"),
+	}
+
+	if err := c.setupSerf(); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+// func	(self *Membership) Write(p []byte) (n int, err error) {
+// 	return 0, nil
+// }
+
+func (self *Membership) setupSerf() error {
+	addr, err := net.ResolveTCPAddr("tcp", self.BindAddr)
+	if err != nil {
+		return err
+	}
+
+	// obtain a default configuration
+	config := serf.DefaultConfig()
+	config.Init()
+
+	config.MemberlistConfig.BindAddr = addr.IP.String()
+	config.MemberlistConfig.BindPort = addr.Port
+
+	self.events = make(chan serf.Event)
+	config.EventCh = self.events
+	config.Tags = self.Tags
+	config.NodeName = self.NodeName
+	// config.LogOutput = self
+
+	// create the agent instance
+	self.serf, err = serf.Create(config)
+	if err != nil {
+		return err
+	}
+
+	go self.eventHandler()
+
+	if self.StartJoinAddrs != nil {
+		_, err = self.serf.Join(self.StartJoinAddrs, true)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (self *Membership) eventHandler() {
+	for e := range self.events {
+		switch e.EventType() {
+		case serf.EventMemberJoin:
+			for _, member := range e.(serf.MemberEvent).Members {
+				if self.isLocal(member) {
+					continue
+				}
+				self.HandleJoin(member)
+			}
+		case serf.EventMemberLeave, serf.EventMemberFailed:
+			{
+				for _, member := range e.(serf.MemberEvent).Members {
+					if self.isLocal(member) {
+						continue
+					}
+					self.HandleLeave(member)
+				}
+			}
+		}
+	}
+}
+
+func (self *Membership) HandleLeave(member serf.Member) {
+	if err := self.handler.Leave(member.Name); err != nil {
+		self.logError(err, "failed to join", member)
+	}
+}
+
+func (self *Membership) logError(err error, msg string, member serf.Member) {
+	self.logger.Error(msg,
+		zap.Error(err),
+		zap.String("name", member.Name),
+		zap.String("rpc_addr", member.Tags["rpc_addr"]))
+}
+
+func (self *Membership) isLocal(member serf.Member) bool {
+	return self.serf.LocalMember().Name == member.Name
+}
+
+func (self *Membership) HandleJoin(member serf.Member) {
+	if err := self.handler.Join(member.Name, member.Tags["rpc_addr"]); err != nil {
+		self.logError(err, "failed to join", member)
+	}
+}
+
+func (self *Membership) Members() []serf.Member {
+	return self.serf.Members()
+}
+
+func (self *Membership) Leave() error {
+	return self.serf.Leave()
+}
